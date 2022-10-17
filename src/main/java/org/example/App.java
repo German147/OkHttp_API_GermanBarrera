19a0e4fe4c1ed2752aef4451879de30282e85c3f
German <barreragerman27@gmail.com>
2022-10-17 16:55:21 -0300

    It was created a MyPost pojo to create a response.

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,39 +1,41 @@
 package org.example;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import okkHttp.MyPost;
 
 import java.io.IOException;
+import java.lang.reflect.Type;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.List;
 
 /**
  * Hello world!
- *
  */
 public class App {
 
     private static final String POST_API_URL = "https://jsonplaceholder.typicode.com/posts";
 
-    public static void main( String[] args ) {
+    public static void main(String[] args) throws IOException, InterruptedException {
 
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
                 .GET()
-                .header("accept","application/json")
+                .header("accept", "application/json")
                 .uri(URI.create(POST_API_URL))
                 .build();
-        try {
-            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
-            System.out.println(response.body());
-        } catch (IOException | InterruptedException e) {
-            throw new RuntimeException(e);
-        }
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
-        ObjectMapper mapper = new ObjectMapper();
+        ObjectMapper mapper = new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);;
 
+        List<MyPost> posts = mapper.readValue(response.body(), new TypeReference<List<MyPost>>(){});
+
+        posts.forEach(System.out::println);
 
     }
 }
